9963906e024e859816e493d59a2182150e187b4f
hkmendo4760 <hkmendo4760@users.noreply.github.com>
2026-02-14 15:52:18 -0800

    created king library php and html

---
+++ php/assignment_2_add_patron.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    <title>Adding Patron</title>
+    <link rel="stylesheet" type="text/css" href="css/basic.css">    
+    </head>
+<body>
+    <div id="logo">
+        <img src="http://profperry.com/Classes20/PHPwithMySQL/KingLibLogo.jpg" alt="King Real Estate Logo">
+    </div>
+
+    <header>
+    <h3> Thank You for Registering!</h3>
+    </header>
+    
+    <?php
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $email = $_POST['email'];
+        $city = $_POST['city'];
+
+        $fullname = $firstname . " " . $lastname;
+              
+        print "<p>Name: $fullname</p>";
+        print "<p>Email: $email</p>";
+        print "<p>City: $city</p>";
+    ?>
+   
+    <footer>
+    <p><br>Copyright &copy; 2026 (Hector Mendoza)</p>
+    </footer>
+</body>
+</html>
